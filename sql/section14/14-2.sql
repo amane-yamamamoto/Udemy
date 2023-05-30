92728baf0ff81c80f3ae2a5c50e94ec29c78a7d1
Amane Yamamoto <yamamoto.amane@hrbrain.co.jp>
2023-05-30 11:33:55 +0900

    finished section14

---
+++ sql/section14/14-2.sql
@@ -0,0 +1,3 @@
+-- 省略パターン
+insert into products values (1002, '新商品B', 2000);
+select * from products;
---
+++ sql/section14/14-7.sql
@@ -0,0 +1,7 @@
+-- カテゴリを使うのをやめるので、消す
+
+select * from products_categories;
+
+delete from products_categories;
+
+select * from products_categories;
